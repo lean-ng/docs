f0d55d4f95b3d7098e8eec979ceec7441521a75d
Micha Alt <micha@haus23.net>
2019-08-28 00:36:45 +0200

    Routing added and home page created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { HomeComponent } from './core/components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/app.routes.ts
@@ -0,0 +1,7 @@
+import { Route } from '@angular/router';
+
+import { HomeComponent } from './core/components/home/home.component';
+
+export const appRoutes: Route[] = [
+  { path: '', component: HomeComponent}
+];
