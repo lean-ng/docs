557380bd65552b50a4a7db42917a26114856a58d
Micha Alt <47160318+lean-ng@users.noreply.github.com>
2019-11-01 21:57:52 +0100

    Create index.js

---
+++ src/assets/docs/cli/index.js
@@ -0,0 +1,13 @@
+## Angular CLI
+
+### Official
+
+### Updates
+
+* [GitHub Repo mit Diffs für alle CLI Versionen](https://github.com/cexbrayat/angular-cli-diff)
+* [Schönes Repo über Diffs, leider nicht mer aktuell](https://github.com/Nolanus/angular-cli-diff)
+
+* [New Features: 8.3](https://blog.ninja-squad.com/2019/08/22/angular-cli-8.3/)
+
+### CLI Builders
+
